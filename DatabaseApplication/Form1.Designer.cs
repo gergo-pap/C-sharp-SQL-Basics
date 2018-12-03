c2f134cee5593113ac287f3b0767407e7a415380
Pap Gergő <p.gergo99@gmail.com>
2018-12-03 14:48:34 +0100

    done

---
+++ DatabaseApplication/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DatabaseApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonHozzaAd = new System.Windows.Forms.Button();
+            this.textBoxNev = new System.Windows.Forms.TextBox();
+            this.textBoxFajta = new System.Windows.Forms.TextBox();
+            this.dateTimePickerOrokbeFogad = new System.Windows.Forms.DateTimePicker();
+            this.buttonTorles = new System.Windows.Forms.Button();
+            this.buttonModositas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonHozzaAd
+            // 
+            this.buttonHozzaAd.Location = new System.Drawing.Point(379, 67);
+            this.buttonHozzaAd.Name = "buttonHozzaAd";
+            this.buttonHozzaAd.Size = new System.Drawing.Size(75, 23);
+            this.buttonHozzaAd.TabIndex = 0;
+            this.buttonHozzaAd.Text = "Hozzáad";
+            this.buttonHozzaAd.UseVisualStyleBackColor = true;
+            this.buttonHozzaAd.Click += new System.EventHandler(this.buttonHozzaAd_Click);
+            // 
+            // textBoxNev
+            // 
+            this.textBoxNev.Location = new System.Drawing.Point(40, 30);
+            this.textBoxNev.Name = "textBoxNev";
+            this.textBoxNev.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNev.TabIndex = 1;
+            // 
+            // textBoxFajta
+            // 
+            this.textBoxFajta.Location = new System.Drawing.Point(147, 29);
+            this.textBoxFajta.Name = "textBoxFajta";
+            this.textBoxFajta.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFajta.TabIndex = 2;
+            // 
+            // dateTimePickerOrokbeFogad
+            // 
+            this.dateTimePickerOrokbeFogad.Location = new System.Drawing.Point(254, 29);
+            this.dateTimePickerOrokbeFogad.Name = "dateTimePickerOrokbeFogad";
+            this.dateTimePickerOrokbeFogad.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerOrokbeFogad.TabIndex = 3;
+            // 
+            // buttonTorles
+            // 
+            this.buttonTorles.Location = new System.Drawing.Point(379, 97);
+            this.buttonTorles.Name = "buttonTorles";
+            this.buttonTorles.Size = new System.Drawing.Size(75, 23);
+            this.buttonTorles.TabIndex = 4;
+            this.buttonTorles.Text = "Törlés";
+            this.buttonTorles.UseVisualStyleBackColor = true;
+            this.buttonTorles.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonModositas
+            // 
+            this.buttonModositas.Location = new System.Drawing.Point(379, 127);
+            this.buttonModositas.Name = "buttonModositas";
+            this.buttonModositas.Size = new System.Drawing.Size(75, 23);
+            this.buttonModositas.TabIndex = 5;
+            this.buttonModositas.Text = "Módosítás";
+            this.buttonModositas.UseVisualStyleBackColor = true;
+            this.buttonModositas.Click += new System.EventHandler(this.buttonModositas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 355);
+            this.Controls.Add(this.buttonModositas);
+            this.Controls.Add(this.buttonTorles);
+            this.Controls.Add(this.dateTimePickerOrokbeFogad);
+            this.Controls.Add(this.textBoxFajta);
+            this.Controls.Add(this.textBoxNev);
+            this.Controls.Add(this.buttonHozzaAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonHozzaAd;
+        private System.Windows.Forms.TextBox textBoxNev;
+        private System.Windows.Forms.TextBox textBoxFajta;
+        private System.Windows.Forms.DateTimePicker dateTimePickerOrokbeFogad;
+        private System.Windows.Forms.Button buttonTorles;
+        private System.Windows.Forms.Button buttonModositas;
+    }
+}
+
---
+++ DatabaseApplication/Form1.cs
@@ -0,0 +1,99 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace DatabaseApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+
+        {
+           
+            InitializeComponent();
+
+            
+        }
+
+        private void buttonHozzaAd_Click(object sender, EventArgs e)
+        {
+            using (var conn = new MySqlConnection("Server=localhost;Database=kutyak;Uid=root;Pwd=;"))
+            {
+                conn.Open();
+                string nev = textBoxNev.Text;
+                string fajta = textBoxFajta.Text;
+                DateTime orokbe = dateTimePickerOrokbeFogad.Value;
+
+                var lekerdezesCommand = conn.CreateCommand();
+                lekerdezesCommand.CommandText = @"
+                    SELECT COUNT(*) FROM kutyak WHERE nev = @nev
+                ";
+                lekerdezesCommand.Parameters.AddWithValue("@nev", nev);
+                long db = (long) lekerdezesCommand.ExecuteScalar();
+                if (db > 0)
+                {
+                    MessageBox.Show("Ilyen kutya már van");
+                    return;
+                }
+                var command = conn.CreateCommand();
+                command.CommandText = @"INSERT INTO 
+                                    kutyak
+                                    (
+                                    nev,
+                                    fajta,
+                                    orokbefogadas_datum
+                                    ) 
+                                    VALUES 
+                                    (
+                                    @nev,
+                                    @fajta,
+                                    @orokbe)";
+                command.Parameters.AddWithValue("@nev", nev);
+                command.Parameters.AddWithValue("@fajta", fajta);
+                command.Parameters.AddWithValue("@orokbe", orokbe);
+                command.ExecuteNonQuery();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxNev.Text != "")
+            {
+                var conn = new MySqlConnection("Server=localhost;Database=kutyak;Uid=root;Pwd=;");
+                conn.Open();
+                string nev = textBoxNev.Text;
+                var torlesCommand = conn.CreateCommand();
+                torlesCommand.CommandText = "Delete from kutyak where nev= @nev";
+                torlesCommand.Parameters.AddWithValue("@nev", nev);
+                torlesCommand.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Record Deleted Successfully!");
+            }
+            else
+            {
+                Console.WriteLine("nem jó");
+            }
+        }
+
+        private void buttonModositas_Click(object sender, EventArgs e)
+        {
+            if (textBoxNev.Text != "")
+            {
+                var conn = new MySqlConnection("Server=localhost;Database=kutyak;Uid=root;Pwd=;");
+                conn.Open();
+                string nev = textBoxNev.Text;
+                var torlesCommand = conn.CreateCommand();
+                torlesCommand.CommandText = "Update kutyak set nev='"+textBoxNev.Text+"', fajta='"+textBoxFajta.Text+"' where nev= @nev";
+                torlesCommand.Parameters.AddWithValue("@nev", nev);
+                torlesCommand.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Record Updated Successfully!");
+            }
+            else
+            {
+                Console.WriteLine("nem jó");
+            }
+        }
+    }
+}
